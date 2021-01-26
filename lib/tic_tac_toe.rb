badf305f71c2c143dab5d3a2b0af8f61274bb8a0
Lena Hou <lenahou2013@gmail.com>
2021-01-26 18:07:19 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -33,10 +33,11 @@ end
 
 #6 Define #position_taken? method
 def position_taken?(board, index)
-  if board[index] == "" || board[index] == " " || board[index] == nil
-    return false
-  else
+  if !(board[index] == "" || board[index] == " " || board[index] == nil)
     return true
+  else
+    return flase
+
   end
 end
 
