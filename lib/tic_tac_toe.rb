7e9c3e58528bdf645450349ea7653c649ed74360
Lena Hou <lenahou2013@gmail.com>
2021-01-26 18:17:58 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -42,7 +42,7 @@ end
 
 #7 Define #valid_move? method
 def valid_move?(board, index)
-   if position_taken?(board, index) && index.between?(0, 8)
+   if position_taken?(board, index) && index.between?(0,8)
     return true
   else
     return false
