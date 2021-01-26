a62ecf1fc4f88e3ec71fd31939c109ce8a67fab3
Lena Hou <lenahou2013@gmail.com>
2021-01-26 18:33:02 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -126,12 +126,12 @@ def winner(board)
   end
 
 def play(board)
-  while !over?(board) && !won?(board) && !draw(board)  #Make turns if the game is not over
+  while !over?(board) && !won?(board) && !draw?(board)  #Make turns if the game is not over
     turn(board)
   end
 
   if won?(board)
-    puts "Congragulations #{winner(board)}"
+    puts "Congragulations #{winner(board)}!"
   end
 
   if draw?(board)
