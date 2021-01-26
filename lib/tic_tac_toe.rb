7cee68088e6b31395cdd9eba696863367b4135b6
Lena Hou <lenahou2013@gmail.com>
2021-01-26 18:19:34 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -74,7 +74,7 @@ def turn(board)
 puts "Please enter 1-9:"
 user_input = gets.strip
 index = input_to_index(user_input)
-if valid_move?(board, index)
+if valid_move?(board, index) == true
   move(board, index, marker)
 return display_board(board)
 else
