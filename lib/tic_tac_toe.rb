4cbc6e4cc1fef220f6e13c98f6f01c0f96678bc5
Lena Hou <lenahou2013@gmail.com>
2021-01-26 17:54:07 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -70,8 +70,8 @@ def turn(board)
 puts "Please enter 1-9:"
 user_input = gets.strip
 index = input_to_index(user_input)
-  if valid_move?(board, index) == true
-  move(board, index, marker)
+if valid_move?(board, index) == true
+  move(board, index, marker = "X")
   display_board(board)
 else
   turn(board)  #here is a recursion
